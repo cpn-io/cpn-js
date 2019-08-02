3d696cef60be46baba3f6778b14e8dfa6b15ce66
indevprof <indevprof@gmail.com>
2019-08-02 14:55:39 +0300

    -- merge

--- cpn-ide-front/src/app/app.version.ts
+++ cpn-ide-front/src/app/app.version.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export class AppVersion { public static readonly buildVersion = '1.1.20190717'; }
-=======
-export class AppVersion { public static readonly buildVersion = '1.1.20190716'; }
->>>>>>> 22ebadf15b59102211fb22dffb83bbe13c589ead
+export class AppVersion { public static readonly buildVersion = '1.1.20190726'; }

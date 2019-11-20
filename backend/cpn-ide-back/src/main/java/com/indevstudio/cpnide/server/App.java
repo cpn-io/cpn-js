06c27ac432c45257f0be12e8c2d25b221cf37a95
semenov-k <k.semenov@indevstudio.com>
2019-11-20 17:21:11 +0300

    add replication remove unneded files

--- backend/cpn-ide-back/src/main/java/com/indevstudio/cpnide/server/App.java
+++ backend/cpn-ide-back/src/main/java/com/indevstudio/cpnide/server/App.java
@@ -2,10 +2,7 @@ package com.indevstudio.cpnide.server;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 
 @SpringBootApplication
--- backend/cpn-ide-back/src/main/java/com/indevstudio/cpnide/server/model/NetInfo.java
+++ backend/cpn-ide-back/src/main/java/com/indevstudio/cpnide/server/model/NetInfo.java
@@ -1,30 +1,15 @@
 package com.indevstudio.cpnide.server.model;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class NetInfo {
     List<String> firedTrans;
     List<String> enableTrans;
     List<PlaceMark> tokensAndMark;
-
-    public List<String> getFiredTrans() {
-        return firedTrans;
-    }
-    public List<String> getEnableTrans() {
-        return enableTrans;
-    }
-
-    public void setEnableTrans(List<String> enableTrans) {
-        this.enableTrans = enableTrans;
-    }
-
-    public List<PlaceMark> getTokensAndMark() {
-        return tokensAndMark;
-    }
-
-    public void setTokensAndMark(List<PlaceMark> tokensAndMark) {
-        this.tokensAndMark = tokensAndMark;
-    }
+    String extraInfo;
 
     public NetInfo(List<String> firedTrans, List<String> enableTrans, List<PlaceMark> tokensAndMark) {
         this.firedTrans = firedTrans;
